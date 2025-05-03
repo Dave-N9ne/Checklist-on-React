de71a889caf86919fdb2be86b1ca13563e0daa7f
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-05-03 20:00:00 +0300

    change components in React.StrictMode component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Checklist from './Checklist';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Checklist />
   </React.StrictMode>
 );
 
