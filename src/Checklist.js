43e1eb9154781316ca9f62b12c73f1f7af3fa6b6
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-05-27 13:47:26 +0300

    add inputs and button for adding new row in the list

--- src/Checklist.js
+++ src/Checklist.js
@@ -24,14 +24,33 @@ const todoList = [
 function Checklist() {
   const [value, setValue] = useState(todoList);
 
+  const [value2, setValue2] = useState(['', '']);
+  const newRow = <div className={styles.adding}>
+    <ul className={styles.adding__list}>
+      {value2.map((element, index) => <li className={styles.adding__item}>
+      <input
+        className={styles.adding__input}
+        type="text"
+        value={element}
+        onChange={(event) => 
+          changeHandler2(index, event)}
+      />
+    </li>)}
+    </ul>
+    <button
+      className={styles.adding__button}
+      onClick={() => 
+        addRow(value2[0], value2[1])}
+    >
+        Add New Row
+    </button>
+  </div>
+
   function editInputs(id, field, event) {
     setValue(value.map((element) => {
       if (element.id === id) {
         element[field] = event.target.value;
       }
-      /* if (!element[field].includes(':')) {
-        element[field] = null;
-      } */
       return element;
     }));
   }
@@ -43,7 +62,8 @@ function Checklist() {
       }
       return previous;
     }))
-    const copy = value.slice();
+
+    const copy = [...value];
     copy.sort(compareFn);
     setValue(copy);
   }
@@ -57,20 +77,44 @@ function Checklist() {
     }))
   }
 
-  return <table className={styles.table}>
-    <Thead
-      styleNumber={styles.number}
-      row={todoList[0]}
-    />
-    <Tbody
-      styleNumber={styles.number} 
-      list={value}
-      value={value}
-      editInputs={editInputs}
-      clickHandler={clickHandler}
-      checkHandler={checkHandler}
-    />
-  </table> // добавить инпут и кнопку для добавления элемента в таблицу и расположить в порядке возрастания по времени 
+  function changeHandler2(index, event) {
+    setValue2([
+      ...value2.slice(0, index),
+      event.target.value,
+      ...value2.slice(index + 1),
+    ])
+  }
+
+  function addRow(value1, value2) {
+    const copy = [...value];
+    copy.push({
+      id: nanoid(),
+      isEdit: false,
+      isChecked: false,
+      time: value1,
+      aim: value2,
+    })
+    copy.sort(compareFn);
+    setValue(copy);
+  }
+
+  return <>
+    <table className={styles.table}>
+      <Thead
+        styleNumber={styles.number}
+        row={todoList[0]}
+      />
+      <Tbody
+        styleNumber={styles.number} 
+        list={value}
+        value={value}
+        editInputs={editInputs}
+        clickHandler={clickHandler}
+        checkHandler={checkHandler}
+      />
+    </table>
+    {newRow}
+  </> // добавить инпут и кнопку для добавления элемента в таблицу и расположить в порядке возрастания по времени 
 }
 
 export default Checklist;
