5c696073ef99b8254eead083a3191f5993ef56f8
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-05-28 19:35:04 +0300

    rewrite a code for adding new items in table

--- src/Control.js
+++ src/Control.js
@@ -1,27 +1,28 @@
 import styles from './Control.module.css';
 
-function Control() {
-    const newRow = <div className={styles.adding}>
-        <ul className={styles.adding__list}>
-          {value2.map((element, index) => <li className={styles.adding__item}>
+function Control({value, controlKeys, addRow, changeHandler}) {
+    return <div className={styles.control}>
+        <ul className={styles.control__list}>
+          {value.map((element, index) => <li className={styles.control__item}>
+          <label>
+            {controlKeys[index]}:
+          </label>
           <input
-            className={styles.adding__input}
+            className={styles.control__input}
             type="text"
             value={element}
             onChange={(event) => 
-              changeHandler2(index, event)}
+              changeHandler(index, event)}
           />
         </li>)}
         </ul>
         <button
-          className={styles.adding__button}
-          onClick={() => 
-            addRow(value2[0], value2[1])}
+          className={styles.control__button}
+          onClick={addRow}
         >
-            Add New Row
+            Add new now
         </button>
     </div>
-    return newRow;
 }
 
 export default Control;
