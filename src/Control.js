8e00369eced3d2fc055539111fabfcf33344cc34
Dave-N9ne <sergeytretiakov2003@gmail.com>
2025-06-11 11:00:43 +0300

    add key for list items of Control component

--- src/Control.js
+++ src/Control.js
@@ -3,13 +3,15 @@ import styles from './Control.module.css';
 function Control({value, controlKeys, addRow, changeHandler}) {
     return <div className={styles.control}>
         <ul className={styles.control__list}>
-          {value.map((element, index) => <li className={styles.control__item}>
+          {value.map((element, index) => <li
+                                          className={styles.control__item}
+                                          key={index} 
+                                         >
           <label>
             {controlKeys[index]}:
           </label>
           <input
             className={styles.control__input}
-            type="text"
             value={element}
             onChange={(event) => 
               changeHandler(index, event)}
